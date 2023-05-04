145f8bc8f325c2b8330337d6ce7f14cd53ae259c
rsa01tur <123996900+rsa01tur@users.noreply.github.com>
2023-05-04 12:18:03 -0300

    Update fd.php

--- fd.php
+++ fd.php
@@ -4,10 +4,10 @@ class Bom implements Fazer {
  
 public function executar(){
 	if (isset($_POST["nome"])){
-    shell_exec('docker run --name teste11 debian -p 500:22');
+    shell_exec('docker run --name teste22 debian -p 500:22');
     }
        if (isset($_POST["nome1"])){
-    shell_exec('docker run --name teste22 debian -p 500:22');
+    shell_exec('docker run --name teste33 debian -p 500:22');
 }
 }}
 
